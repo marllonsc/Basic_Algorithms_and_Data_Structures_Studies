a8599fb987c20e4c05db06d370cf34e345c7caed
Marllon Dos Santos Costa <marllon@ibm.com>
2023-05-29 10:54:49 -0300

    add 5725T26

--- src/com/cpq/helper/App_5725T26.java
+++ src/com/cpq/helper/App_5725T26.java
@@ -4,5 +4,162 @@ public class App_5725T26 extends CpqHelper{
 
 	public static void main(String[] args) {
 		
+//		String and1 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud Enterprise Authorized User Advanced Support"
+//				,"Global_SaaSESupport.E0EFVZX",
+//				"TRIRIGABaseSubscriptionUsers.Authorized Users",
+//				"TRIRIGABaseSubscriptionUsers:AuthorizedUsers");
+		
+		
+		String and2 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud Enterprise Authorized User Advanced Support"
+				,"Global_SaaSESupport:E0E5TZX",
+				"TRIRIGABaseSubscriptionUsers.Concurrent Users",
+				"TRIRIGABaseSubscriptionUsers:ConcurrentUsers");
+		
+		
+		String and3 = execute("Global_SaaSESupport.TRIRIGA Connector for Esri ArcGIS Indoors SaaS for US Federal - 2,350 SQM Item Advanced Support"
+				,"Global_SaaSESupport:E0E3MZX",
+				"TRIRIGAConnectorforEsriArcGISIndoors.Square Meters",
+				"TRIRIGAConnectorforEsriArcGISIndoors:SquareMeters");
+		
+		String and4 = execute("Global_SaaSESupport.TRIRIGA Connector for Esri ArcGIS Indoors SaaS- 2,350 SQM Item Advanced Support"
+				,"Global_SaaSESupport:E0E3PZX",
+				"TRIRIGAConnectorforEsriArcGISIndoors.Square Meters",
+				"TRIRIGAConnectorforEsriArcGISIndoors:SquareMeters");
+		
+		String and5 = execute("Global_SaaSESupport.TRIRIGA Connector for Esri ArcGIS Indoors SaaS- 25,000 SQ FT Item Advanced Support"
+				,"Global_SaaSESupport:E0E3QZX",
+				"TRIRIGAConnectorforEsriArcGISIndoors.Square Feet",
+				"TRIRIGAConnectorforEsriArcGISIndoors:SquareFeet");
+		
+		String and6 = execute("Global_SaaSESupport.TRIRIGA Connector for Esri ArcGIS Indoors SaaS for US Federal - 25,000 SQ FT Item Advanced Support"
+				,"Global_SaaSESupport:E0E3NZX",
+				"TRIRIGAConnectorforEsriArcGISIndoors.Square Feet",
+				"TRIRIGAConnectorforEsriArcGISIndoors:SquareFeet");
+		
+		String and7 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud Enhanced Disaster Recovery fr US Federal FedRamp Non-Production Instance Moderate Add-On Instance Advanced Support"
+				,"Global_SaaSESupport:E0EC2ZX",
+				"TRIRIGAFedRampAdd-on.Non Production Instance - Moderate",
+				"TRIRIGAFedRampAdd-on:NonProductionInstanceModerate");
+		
+		String and8 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud Enhanced Disaster Recovery for US Federal FedRamp Non-Production Instance High Add-On Instance Advanced Support"
+				,"Global_SaaSESupport:E0EC3ZX",
+				"TRIRIGAFedRampAdd-on.Non Production Instance - High",
+				"TRIRIGAFedRampAdd-on:NonProductionInstanceHigh");
+		
+		String and9 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud for US Federal Occasional Enterprise Authorized User Advanced Support"
+				,"Global_SaaSESupport:E0E9QZX",
+				"TRIRIGAOptionalUsers.Occasional Enterprise Authorized User",
+				"TRIRIGAOptionalUsers:OccasionalEnterpriseAuthorizedUser");
+		
+		String and10 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud for US Federal Approvals and Reporting Authorized User Advanced Support"
+				,"Global_SaaSESupport:E0E8YZX",
+				"TRIRIGAOptionalUsers.Approvals and Reporting Authorized User",
+				"TRIRIGAOptionalUsers:ApprovalsandReportingAuthorizedUser");
+		
+		String and11 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud for US Federal Self-Service Authorized User Advanced Support"
+				,"Global_SaaSESupport:E0E8VZX",
+				"TRIRIGAOptionalUsers.Self Service Authorized User",
+				"TRIRIGAOptionalUsers:SelfServiceAuthorizedUser");
+		
+		String and12 = execute("Global_SaaSESupport.TRIRIGA Capital Projects Hub SaaS Federal Authorized User Advanced Support"
+				,"Global_SaaSESupport:E0E3LZX",
+				"TRIRIGAOptionalUsers.Capital Projects Hub Authorized User",
+				"TRIRIGAOptionalUsers:CapitalProjectsHubAuthorizedUser");
+		
+		
+		String and13 = execute("Global_SaaSESupport.TRIRIGA Capital Projects Hub SaaS Federal Concurrent User Advanced Support"
+				,"Global_SaaSESupport:E0E3KZX",
+				"TRIRIGAOptionalUsers.Capital Projects Hub Concurrent User",
+				"TRIRIGAOptionalUsers:CapitalProjectsHubConcurrentUser");
+		
+		String and14 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud Occasional Enterprise Authorized User Advanced Support"
+				,"Global_SaaSESupport:E0E4VZX",
+				"TRIRIGAOptionalUsers.Capital Projects Hub Concurrent User",
+				"TRIRIGAOptionalUsers:CapitalProjectsHubConcurrentUser");
+		
+		String and15 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud for US Federal Add-on Capacity for Production Instance Advanced Support"
+				,"Global_SaaSESupport:E0E3IZX",
+				"TRIRIGAOptionalAddon.Add-on Capacity for Production",
+				"TRIRIGAOptionalAddon:AddonCapacityforProduction");
+		
+		String and16 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud for US Federal Baremetal Server Deployment Instance Advanced Support"
+				,"Global_SaaSESupport:E0E3EZX",
+				"TRIRIGAOptionalAddon.Baremetal Server Deployment",
+				"TRIRIGAOptionalAddon:BaremetalServerDeployment");
+		
+		String and17 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud for US Federal SFTP Accounts Instance Advanced Support"
+				,"Global_SaaSESupport:E0E3FZX",
+				"TRIRIGAOptionalAddon.SFTP Accounts",
+				"TRIRIGAOptionalAddon:SFTPAccounts");
+		
+		String and18 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud for US Federal Add-on VPN IPSec Tunnel Setup Instance Advanced Support"
+				,"Global_SaaSESupport:E0E3GZX",
+				"TRIRIGAOptionalAddon.Add-on VPN IPSec Tunnel Setup",
+				"TRIRIGAOptionalAddon:AddonVPNIPSecTunnelSetup");
+		
+		String and19 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud for US Federal IP Whitelisting Instance Advanced Support"
+				,"Global_SaaSESupport:E0E3HZX",
+				"TRIRIGAOptionalAddon.IP Whitelisting",
+				"TRIRIGAOptionalAddon:IPWhitelisting");
+		
+		
+		String and20 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud US Federal Advanced Distribution Recovery Add-on Instance Advanced Support"
+				,"Global_SaaSESupport:E0ECRZX",
+				"TRIRIGAOptionalAddon.Enhanced Disaster Recovery Add-on",
+				"TRIRIGAOptionalAddon:EnhancedDisasterRecoveryAddon");
+		
+		String and21 = execute("Global_SaaSESupport.TRIRIGA Real Estate and Facilities Management SaaS for US Federal Add-on Reporting Instance Advanced Support"
+				,"Global_SaaSESupport:E0E3SZX",
+				"TRIRIGAOptionalAddon.Add-On Reporting Production Instance",
+				"TRIRIGAOptionalAddon:AddOnReportingProductionInstance");
+		
+		String and22 = execute("Global_SaaSESupport.TRIRIGA Real Estate and Facilities Management SaaS for US Federal Add-on Reporting Instance Advanced Support"
+				,"Global_SaaSESupport:E0E3VZX",
+				"TRIRIGAOptionalAddon.Add-On Reporting Non-Production Instance",
+				"TRIRIGAOptionalAddon:AddOnReportingNonProductionInstance");
+		
+		String and23 = execute("Global_SaaSESupport.TRIRIGA Assistant 50 Concurrent Users Advanced Support"
+				,"Global_SaaSESupport:E0E3JZX",
+				"TRIRIGAOptionalAddon.Add-On TRIRIGA Assistant",
+				"TRIRIGAOptionalAddon:AddOnTRIRIGAAssistant");
+		
+		String and24 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud for US Federal FedRamp Moderate Add-On Instance Advanced Support"
+				,"Global_SaaSESupport:E0EBVZX",
+				"TRIRIGAFedRamp.Moderate",
+				"TRIRIGAFedRamp:Moderate");
+		
+		String and25 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud for US Federal FedRamp High Add-On Instance Advanced Support"
+				,"Global_SaaSESupport:E0EBWZX",
+				"TRIRIGAFedRamp.High",
+				"TRIRIGAFedRamp:High");
+		
+
+		String and26 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud for US Federal Additional Non-Production Instance Advanced Support"
+				,"Global_SaaSESupport:E0EBSZX",
+				"TRIRIGANonProductionAddon.Additional Non-Production Instance (up to 30 concurrent users)",
+				"TRIRIGANonProductionAddon:AdditionalNonProductionInstance");
+		
+		String and27 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud Additional Non-Production Instance Advanced Support"
+				,"Global_SaaSESupport:E0E9SZX",
+				"TRIRIGANonProductionAddon.Additional Non-Production Instance (up to 30 concurrent users)",
+				"TRIRIGANonProductionAddon:AdditionalNonProductionInstance");
+		
+		
+		String and29 = execute("Global_SaaSESupport.Facilities and Real Estate Management (TRIRIGA) on Cloud for US Federal Availability for Non-Production Systems Instance Advanced Support"
+				,"Global_SaaSESupport:E0E3DZX",
+				"TRIRIGAOptionalAddon2.Availability for Non-Production Systems",
+				"TRIRIGAOptionalAddon2:AvailabilityforNonProductionSystems");
+		
+		String and30 = execute("Global_SaaSESupport.TRIRIGA SaaS Restricted Operational Access Add-on Instance Advanced Support"
+				,"Global_SaaSESupport:E0E3UZX",
+				"TRIRIGAOptionalAddon2.Restricted Operational Access",
+				"TRIRIGAOptionalAddon2:RestrictedOperationalAccess");
+		
+		
+		String checking = or(and2, or(and3, or(and4, or(and5, or(and6, or(and7, or(and8, or(and9, or(and10, or(and11, or(and12, or(and13, or(and14, or(and15, or(and16
+				, or(and17, or(and18, or(and19, or(and20, or(and21, or(and22, or(and23, or(and24, or(and25, or(and26, or(and27, or(and29, and30)))))))))))))))))))))))))));
+		
+		
+		System.out.println(checking);
 	}
 }
